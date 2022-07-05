844a98c0c5028d9da6728ef81857384033ed4804
kimsubeen <papajj06@naver.com>
2022-07-05 17:05:09 +0900

    example

--- app/src/main/java/com/teampome/pome/MainActivity.kt
+++ app/src/main/java/com/teampome/pome/MainActivity.kt
@@ -2,10 +2,12 @@ package com.teampome.pome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.d("example")
     }
 }
