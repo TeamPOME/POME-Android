b783a97c1dd342cea2f2faeb3aa51efd86a3c380
kimsubeen <papajj06@naver.com>
2022-07-06 17:08:45 +0900

    [feature/#4] icon추가 구현

--- app/src/main/java/com/teampome/pome/MainActivity.kt
+++ app/src/main/java/com/teampome/pome/MainActivity.kt
@@ -2,12 +2,33 @@ package com.teampome.pome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.skydoves.balloon.*
+import com.teampome.pome.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val iconBalloon by balloon<FriendPageIconFactory>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Timber.d("example")
+        iconClickEvent()
+
+    }
+
+    private fun iconClickEvent() {
+        binding.ivIcon.setOnClickListener {
+            iconBalloon.showAlignBottom(it)
+        }
+        val button: ImageView =
+            iconBalloon.getContentView().findViewById(R.id.iv_first)
+        button.setOnClickListener {
+            binding.ivIcon.setImageResource(R.drawable.ic_what_mint)
+            iconBalloon.dismiss()
+        }
+
     }
 }
