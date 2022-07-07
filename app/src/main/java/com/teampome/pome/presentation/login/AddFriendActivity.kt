47d7a74d7c7d02ae4a1ccbde9391d82dfbb7b639
kimsubeen <papajj06@naver.com>
2022-07-08 02:57:10 +0900

    [feature/#12] addfriend 뷰 틀 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/login/AddFriendActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class AddFriendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friend)
+    }
+}
