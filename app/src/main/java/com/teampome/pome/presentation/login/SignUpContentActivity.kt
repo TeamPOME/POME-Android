6c99ffeabb35b015e91e1a6bb0018e55254620d3
kimsubeen <papajj06@naver.com>
2022-07-07 16:14:31 +0900

    [feature/#10] 친구 추가 설명 뷰 구현

--- app/src/main/java/com/teampome/pome/presentation/login/SignUpContentActivity.kt
+++ app/src/main/java/com/teampome/pome/presentation/login/SignUpContentActivity.kt
@@ -1,11 +1,12 @@
-package com.teampome.pome
+package com.teampome.pome.presentation.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.teampome.pome.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpContentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_sign_up_content)
     }
 }
