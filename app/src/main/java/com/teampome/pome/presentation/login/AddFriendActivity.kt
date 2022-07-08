d66bdc90315523d344b88d9db59104d906a17da2
kimsubeen <papajj06@naver.com>
2022-07-08 18:52:28 +0900

    [feature/#12] 친구추가뷰 Adapter

--- app/src/main/java/com/teampome/pome/presentation/login/AddFriendActivity.kt
+++ app/src/main/java/com/teampome/pome/presentation/login/AddFriendActivity.kt
@@ -2,11 +2,52 @@ package com.teampome.pome.presentation.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.teampome.pome.R
+import com.teampome.pome.databinding.ActivityAddFriendBinding
+import com.teampome.pome.databinding.ActivitySignUpBinding
+import com.teampome.pome.databinding.ActivitySignUpContentBinding
 
 class AddFriendActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddFriendBinding
+    private var addFriendAdapter = AddFriendAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_friend)
+        binding = ActivityAddFriendBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        binding.rcvFriend.adapter = addFriendAdapter
+        addFriendList()
+    }
+
+    private fun addFriendList() {
+        addFriendAdapter.submitList(
+            listOf(
+                AddFriendData(
+                    R.drawable.ic_friend_profile, "미미"
+                ),
+                AddFriendData(
+                    R.drawable.ic_friend_profile, "미미짱"
+                ),
+                AddFriendData(
+                    R.drawable.ic_friend_profile, "미미파"
+                ),
+                AddFriendData(
+                    R.drawable.ic_friend_profile, "미미리"
+                ),
+                AddFriendData(
+                    R.drawable.ic_friend_profile, "미미쨩"
+                ),
+                AddFriendData(
+                    R.drawable.ic_friend_profile, "미미공주"
+                ),
+
+
+
+            )
+        )
     }
 }
