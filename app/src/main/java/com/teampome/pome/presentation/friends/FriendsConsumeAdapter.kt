49b984277c4a47c09b4f870cd603da509edd9702
yangjiyoung <0112jyoung@naver.com>
2022-07-09 21:24:01 +0900

    [feature/#18] 친구뷰 Adapter 설정

---
+++ app/src/main/java/com/teampome/pome/presentation/friends/FriendsConsumeAdapter.kt
@@ -0,0 +1,54 @@
+package com.teampome.pome.presentation.friends
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teampome.pome.databinding.ItemFriendConsumeListBinding
+
+class FriendsConsumeAdapter :
+    ListAdapter<FriendsConsumeData, FriendsConsumeAdapter.FriendsConsumeViewHolder>(
+        DIFFUTIL
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendsConsumeViewHolder {
+        val binding=ItemFriendConsumeListBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false)
+        return FriendsConsumeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FriendsConsumeViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class FriendsConsumeViewHolder(private val binding: ItemFriendConsumeListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(friendsConsumeData: FriendsConsumeData) {
+            binding.tvFriendname.text = friendsConsumeData.name
+            binding.tvFrienddate.text = friendsConsumeData.date
+            binding.tvFrienddes.text = friendsConsumeData.description
+            binding.tvFriendprice.text = friendsConsumeData.price
+            //프로필 이미지, 반응들, 처음감정과 나중감정
+        }
+
+    }
+
+
+    companion object {
+        val DIFFUTIL = object : DiffUtil.ItemCallback<FriendsConsumeData>() {
+            override fun areItemsTheSame(
+                oldItem: FriendsConsumeData,
+                newItem: FriendsConsumeData
+            ): Boolean = oldItem.name == newItem.name
+
+            override fun areContentsTheSame(
+                oldItem: FriendsConsumeData,
+                newItem: FriendsConsumeData
+            ): Boolean = oldItem == newItem
+
+        }
+    }
+
+
+}
