1a2c2b7bb767530878a23f64f5a84d4b0c4f739b
yangjiyoung <0112jyoung@naver.com>
2022-07-09 20:57:24 +0900

    [feature/#18] 친구 아이템 뷰 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/friends/FriendsConsumeData.kt
@@ -0,0 +1,11 @@
+package com.teampome.pome.presentation.friends
+
+data class FriendsConsumeData(
+    val name:String,
+    val date:String,
+    val price:String, //나중에 int로 받는지 확인하기
+    val description:String,
+    val first_emotion:Int,
+    val second_emotion:Int
+    //친구들 반응 리스트는 나중에
+)
