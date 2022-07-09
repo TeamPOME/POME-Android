1a6ac69973169f3f4dedbf694cf82ebf390b6660
yangjiyoung <0112jyoung@naver.com>
2022-07-09 17:33:12 +0900

    [feature/#18] baseFragment 적용

--- app/src/main/java/com/teampome/pome/presentation/friends/FriendsFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/friends/FriendsFragment.kt
@@ -6,15 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.teampome.pome.R
+import com.teampome.pome.databinding.FragmentFriendsBinding
+import com.teampome.pome.util.BaseFragment
 
-class FriendsFragment : Fragment() {
+class FriendsFragment: BaseFragment<FragmentFriendsBinding>(R.layout.fragment_friends) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
