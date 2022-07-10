a0a0545bcc38be6932b1c389ccb8d9e84ebffba4
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-11 01:39:26 +0900

    [feature/#17] RecyclerView itemDecorator 추가

---
+++ app/src/main/java/com/teampome/pome/util/VerticalItemDecorator.kt
@@ -0,0 +1,20 @@
+package com.teampome.pome.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class VerticalItemDecorator(private val divHeight: Int) : RecyclerView.ItemDecoration() {
+
+    @Override
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = divHeight
+        outRect.bottom = divHeight
+    }
+}
