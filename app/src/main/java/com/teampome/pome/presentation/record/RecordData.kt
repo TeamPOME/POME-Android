f6653c2f1b32df2d2dc26d925508f3d8a2086c38
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-11 01:41:00 +0900

    [feature/#17] RecyclerView 데이터 형식 추가

---
+++ app/src/main/java/com/teampome/pome/presentation/record/RecordData.kt
@@ -0,0 +1,9 @@
+package com.teampome.pome.presentation.record
+
+data class RecordData(
+    val id: Int,
+    val date: String,
+    val firstEmotion: Int,
+    val amount: String,
+    val description: String
+)
