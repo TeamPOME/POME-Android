a946a2c5491a90b96416693f4fe84dccd6c5feb5
kimsubeen <papajj06@naver.com>
2022-07-11 01:09:33 +0900

    [feature/#20] 감정 기록 뷰, 기록 선택완료 뷰 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/record/emotion/AfterCompleteEmotionActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.record.emotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class AfterCompleteEmotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_complete_emotion)
+    }
+}
---
+++ app/src/main/java/com/teampome/pome/presentation/record/emotion/AfterSelectEmotionActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.record.emotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class AfterSelectEmotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_select_emotion)
+    }
+}
