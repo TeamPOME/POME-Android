2f2971fba4ab9f3899fc5b48a1fcce874264c447
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-11 01:42:07 +0900

    [feature/#17] RecyclerView adapter 생성

---
+++ app/src/main/java/com/teampome/pome/presentation/record/RecordAdapter.kt
@@ -0,0 +1,43 @@
+package com.teampome.pome.presentation.record
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teampome.pome.databinding.ItemRecordListBinding
+
+class RecordAdapter : ListAdapter<RecordData, RecordAdapter.RecordViewHolder>(DiffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+        val binding = ItemRecordListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RecordViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
+        holder.onBind(currentList[position])
+    }
+
+    class RecordViewHolder(
+        private val binding: ItemRecordListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: RecordData){
+            binding.tvDate.text = data.date
+            binding.tvAmount.text = data.amount
+            binding.tvDescription.text = data.description
+        }
+    }
+
+    companion object {
+        val DiffUtil = object : DiffUtil.ItemCallback<RecordData>() {
+            override fun areItemsTheSame(oldItem: RecordData, newItem: RecordData): Boolean {
+                return oldItem.id == newItem.id
+            }
+            override fun areContentsTheSame(oldItem: RecordData, newItem: RecordData): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+
+}
