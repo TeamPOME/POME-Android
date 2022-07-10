39a285b70148f7fd909b2590bed3675e30c3d13c
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-11 01:42:51 +0900

    [feature/#17] RecyclerView Fragment에 추가

--- app/src/main/java/com/teampome/pome/presentation/record/RecordFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/RecordFragment.kt
@@ -6,55 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.teampome.pome.R
+import com.teampome.pome.databinding.FragmentRecordBinding
+import com.teampome.pome.util.CustomItemDecorator
+import com.teampome.pome.util.VerticalItemDecorator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RecordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentRecordBinding? = null
+    private val binding get() = _binding ?: error("Binding 초기화 x")
+    private lateinit var recordAdapter: RecordAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false)
+        _binding = FragmentRecordBinding.inflate(layoutInflater, container, false)
+
+        initAdapter()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initAdapter() {
+        recordAdapter = RecordAdapter()
+        binding.rvRecord.adapter = recordAdapter
+        addList()
+        binding.rvRecord.addItemDecoration(CustomItemDecorator(12))
+        binding.rvRecord.addItemDecoration(VerticalItemDecorator(6))
+    }
+
+    private fun addList() {
+        recordAdapter.submitList(
+            listOf(
+                RecordData(1,"06.24", 1, "10,000원", "gg"),
+                RecordData(1,"06.25", 1, "100,000원", "gg"),
+                RecordData(1,"06.26", 1, "200,000원", "gg"),
+                RecordData(1,"06.27", 1, "300,000원", "gg"),
+                RecordData(1,"06.28", 1, "500,000원", "gg"),
+                RecordData(1,"06.29", 1, "1,000,000원", "gg")
+            )
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
