ef477355a11acbb75487491aa0d63a2192bc3109
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-11 16:58:02 +0900

    [layout/#22] 기록 씀씀이 뷰 UI 생성

---
+++ app/src/main/java/com/teampome/pome/presentation/record/RecordLookBackActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class RecordLookBackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_look_back)
+    }
+}
