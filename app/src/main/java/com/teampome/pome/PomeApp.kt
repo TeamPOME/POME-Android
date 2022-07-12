8f052d26512f0a3ad5952243c58e8957fd786acd
kimsubeen <papajj06@naver.com>
2022-07-12 19:03:14 +0900

    [feature/#27] 소셜로그인 환경 세팅

--- app/src/main/java/com/teampome/pome/PomeApp.kt
+++ app/src/main/java/com/teampome/pome/PomeApp.kt
@@ -1,8 +1,12 @@
 package com.teampome.pome
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
 class PomeApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+       // KakaoSdk.init(this, BuildConfig.APIKEY)
+    }
 }
