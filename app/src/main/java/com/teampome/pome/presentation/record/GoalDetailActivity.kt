ce61833ff4fda914e2a13f44b4c4e8b8ed842367
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-12 15:22:44 +0900

    [layout/#25] 목표 디테일 추가 뷰 UI 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/record/GoalDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class GoalDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_detail)
+    }
+}
