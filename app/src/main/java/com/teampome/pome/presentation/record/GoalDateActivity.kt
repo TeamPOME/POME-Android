117b53742c13b33eb1e8b241d3d3272a8b20ab4b
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-12 15:08:43 +0900

    [layout/#25] 목표 날짜 추가 뷰 UI 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/record/GoalDateActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class GoalDateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_date)
+    }
+}
