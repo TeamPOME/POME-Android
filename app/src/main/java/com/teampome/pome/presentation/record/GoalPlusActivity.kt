389387dbc07e9c400143c95a1fa8620408d8f736
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-12 18:23:35 +0900

    [layout/#25] 목표추가 작성완료 뷰 UI 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/record/GoalPlusActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class GoalPlusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_plus)
+    }
+}
