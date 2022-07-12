d57843e4278622e180303855c7b63ca55aa1c16f
kimsubeen <papajj06@naver.com>
2022-07-13 02:49:11 +0900

    [feature/#27] debug key 설정을 위한 세팅

--- app/src/main/java/com/teampome/pome/PomeApp.kt
+++ app/src/main/java/com/teampome/pome/PomeApp.kt
@@ -1,12 +1,15 @@
 package com.teampome.pome
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
-import dagger.hilt.android.HiltAndroidApp
+
 
 class PomeApp : Application() {
     override fun onCreate() {
         super.onCreate()
-       // KakaoSdk.init(this, BuildConfig.APIKEY)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        //KakaoSdk.init(this, "{NATIVE_APP_KEY}")
+        //-> KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
     }
 }
