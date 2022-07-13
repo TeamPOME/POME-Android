7e7cef11371edd3822878667cb90c3a7277b338d
yangjiyoung <0112jyoung@naver.com>
2022-07-13 20:47:46 +0900

    [feature/#32] 회고 기록 adapter 연결

--- app/src/main/java/com/teampome/pome/presentation/remind/RemindFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/remind/RemindFragment.kt
@@ -7,10 +7,38 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentRemindBinding
+import com.teampome.pome.presentation.friends.FriendsConsumeData
 import com.teampome.pome.util.BaseFragment
 
 class RemindFragment : BaseFragment<FragmentRemindBinding>(R.layout.fragment_remind) {
+    private lateinit var remindConsumeAdapter: RemindConsumeAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initRemindConsumeAdapter()
+
+        //initAddRemindConsume()
+    }
+
+    private fun initRemindConsumeAdapter(){
+        remindConsumeAdapter= RemindConsumeAdapter()
+        binding.rvRemind.adapter=remindConsumeAdapter
+    }
+
+    private fun initAddRemindConsume(){
+        remindConsumeAdapter.submitList(
+            listOf(
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713"),
+                FriendsConsumeData(name="dwd", description = "와플은 맛잇다", price = "3900", tag = "간식", first_emotion = 1, second_emotion = 2, date = "0713")
+
+            )
+        )
     }
 }
