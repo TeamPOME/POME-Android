b603795c7fe5df6c52bb13e1b77ba01392038655
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-14 01:27:58 +0900

    [feature/#33] 뒤로가기 로직 추가

--- app/src/main/java/com/teampome/pome/presentation/record/screens/RecordWriteActivity.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/screens/RecordWriteActivity.kt
@@ -3,10 +3,23 @@ package com.teampome.pome.presentation.record.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.teampome.pome.R
+import com.teampome.pome.databinding.ActivityRecordWriteBinding
 
 class RecordWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecordWriteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_record_write)
+        binding = ActivityRecordWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        goBack()
+    }
+
+    private fun goBack() {
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
