49c0122e0f2c6a5d78265f4dfd854513ff161da9
yangjiyoung <0112jyoung@naver.com>
2022-07-13 22:27:48 +0900

    [feature/#32] 회고 기록 bottomsheet 재수정 - 처음감정

---
+++ app/src/main/java/com/teampome/pome/presentation/remind/RemindFirstBottomSheetFragment.kt
@@ -0,0 +1,34 @@
+package com.teampome.pome.presentation.remind
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.teampome.pome.databinding.FragmentRemindFirstBottomSheetBinding
+
+class RemindFirstBottomSheetFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentRemindFirstBottomSheetBinding? = null
+    private val binding get() = _binding ?: error("binding이 되지 않았습니다")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRemindFirstBottomSheetBinding.inflate(layoutInflater, container, false)
+
+        initHeight()
+
+        return binding.root
+    }
+
+    private fun initHeight() {
+        val bottomSheet = binding.clRemindFirstBottomsheet
+        bottomSheet.minHeight = 259
+        bottomSheet.maxHeight = 259
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/teampome/pome/presentation/remind/RemindFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/remind/RemindFragment.kt
@@ -10,15 +10,16 @@ import com.teampome.pome.util.FriendsConsumeItemDecorator
 
 class RemindFragment : BaseFragment<FragmentRemindBinding>(R.layout.fragment_remind) {
     private lateinit var remindConsumeAdapter: RemindConsumeAdapter
-    private val bottomSheetFragment = RemindBottomSheetFragment()
+    private val bottomSheetFragment = RemindFirstBottomSheetFragment()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initRemindConsumeAdapter()
         initAdapterDecoration()
-        initAddRemindConsume() //서버통신할 코드
-        showBottomSheet()
+        initAddRemindConsume()
+
+        initClickFirstEmotion()
     }
 
     private fun initAdapterDecoration() {
@@ -110,18 +111,12 @@ class RemindFragment : BaseFragment<FragmentRemindBinding>(R.layout.fragment_rem
         )
     }
 
-    private fun showBottomSheet() {
-        //서버 통신을 통해 반응이 있는 경우만 보여주기
-        remindConsumeAdapter.setReactionClickListener(object :
-            RemindConsumeAdapter.ReactionClickListener {
-            override fun onClick(data: View, pos: Int) {
-                if (!bottomSheetFragment.isAdded)
-                    bottomSheetFragment.show(
-                        childFragmentManager, bottomSheetFragment.tag
-                    )
-            }
-
-        })
-
+    private fun initClickFirstEmotion() {
+        binding.ivFirstEmotion.setOnClickListener {
+            if (!bottomSheetFragment.isAdded)
+                bottomSheetFragment.show(
+                    childFragmentManager, bottomSheetFragment.tag
+                )
+        }
     }
 }
