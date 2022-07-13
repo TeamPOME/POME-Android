6e54fea5d38a83282c35fe9e97da37bf32d0f751
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-13 15:20:12 +0900

    [feature/#25] 바텀시트 커스텀

---
+++ app/src/main/java/com/teampome/pome/presentation/record/CalendarBottomSheet.kt
@@ -0,0 +1,70 @@
+package com.teampome.pome.presentation.record
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.CalendarMode
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter
+import com.teampome.pome.R
+import com.teampome.pome.databinding.FragmentCalendarBottomSheetBinding
+import com.teampome.pome.util.MinMaxDecorator
+import com.teampome.pome.util.TodayDecorator
+import java.time.LocalDate
+import java.util.*
+
+class CalendarBottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentCalendarBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCalendarBottomSheetBinding.inflate(layoutInflater, container, false)
+        calendarSetting()
+        return binding.root
+    }
+
+    private fun calendarSetting() {
+        binding.mcCalendar.selectedDate = CalendarDay.today()
+        val startTimeCalendar = Calendar.getInstance()
+        val endTimeCalendar = Calendar.getInstance()
+
+        val currentYear = startTimeCalendar.get(Calendar.YEAR)
+        val currentMonth = startTimeCalendar.get(Calendar.MONTH)
+        val currentDate = startTimeCalendar.get(Calendar.DATE)
+
+        //끝나는 달
+        endTimeCalendar.set(Calendar.MONTH, currentMonth + 1)
+
+        //최소 선택 날짜, 최대 선택 날짜
+        binding.mcCalendar.state().edit()
+            .setFirstDayOfWeek(Calendar.SUNDAY)
+            .setMinimumDate(CalendarDay.from(currentYear, currentMonth, 1))
+            .setMaximumDate(
+                CalendarDay.from(
+                    currentYear, currentMonth + 1, endTimeCalendar.getActualMaximum(
+                        Calendar.DAY_OF_MONTH
+                    )
+                )
+            )
+            .setCalendarDisplayMode(CalendarMode.MONTHS)
+            .commit()
+
+        //월, 요일 한글로 보이도록
+        binding.mcCalendar.setTitleFormatter(MonthArrayTitleFormatter(resources.getTextArray(R.array.custom_months)))
+        binding.mcCalendar.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
+
+        val stCalendarDay = CalendarDay.from(currentYear, currentMonth, currentDate)
+        val enCalendarDay = CalendarDay.from(endTimeCalendar.get(Calendar.YEAR), endTimeCalendar.get(Calendar.MONTH), endTimeCalendar.get(Calendar.DATE))
+
+        val minMaxDecorator = MinMaxDecorator(stCalendarDay, enCalendarDay)
+        val todayDecorator = TodayDecorator(requireContext())
+        binding.mcCalendar.addDecorators(minMaxDecorator, todayDecorator)
+    }
+}
