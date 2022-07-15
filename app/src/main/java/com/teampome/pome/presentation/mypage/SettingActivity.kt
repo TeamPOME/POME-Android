f27fb6c4b5fb0d04bc6f3c8e4f45f598f5cdf8a8
kimsubeen <papajj06@naver.com>
2022-07-16 01:05:00 +0900

    [feature/#37] setting 임시 뷰 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/mypage/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.teampome.pome.presentation.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teampome.pome.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
