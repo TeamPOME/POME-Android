9b28d0a311ac566264faa3a0f3d7543c9e2b8f5a
kimsubeen <papajj06@naver.com>
2022-07-15 18:51:12 +0900

    [feature/#37] mypage 리사이클러뷰 추가
    더미데이터

--- app/src/main/java/com/teampome/pome/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/mypage/MyPageFragment.kt
@@ -9,10 +9,41 @@ import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentMyPageBinding
 import com.teampome.pome.databinding.FragmentRecordBinding
 import com.teampome.pome.util.base.BaseFragment
+import com.teampome.pome.util.decorate.VerticalItemDecorator
 
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private var marshMellowAdapter = MarshMellowAdapter()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        initAdapter()
     }
+
+    private fun initAdapter() {
+        binding.rcvMarsh.adapter = marshMellowAdapter
+        addMarshList()
+
+    }
+
+    private fun addMarshList() {
+        marshMellowAdapter.submitList(
+            listOf(
+                MarshMellowData(
+                    R.drawable.ic_level, R.drawable.ic_badge, R.drawable.ic_myprofile
+                ),
+                MarshMellowData(
+                    R.drawable.ic_level, R.drawable.ic_badge, R.drawable.ic_myprofile
+                ),
+                MarshMellowData(
+                    R.drawable.ic_level, R.drawable.ic_badge, R.drawable.ic_myprofile
+                ),
+                MarshMellowData(
+                    R.drawable.ic_level, R.drawable.ic_badge, R.drawable.ic_myprofile
+                )
+            )
+        )
+
+    }
+
 }
