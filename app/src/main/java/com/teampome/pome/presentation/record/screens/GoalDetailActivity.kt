8599d449a950df824638c0e11381bde6ab643562
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-17 02:03:26 +0900

    [feature/#45] edittext 다 채워졌을 경우 버튼 활성화되는 로직 생성

--- app/src/main/java/com/teampome/pome/presentation/record/screens/GoalDetailActivity.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/screens/GoalDetailActivity.kt
@@ -3,18 +3,25 @@ package com.teampome.pome.presentation.record.screens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.teampome.pome.databinding.ActivityGoalDetailBinding
+import com.teampome.pome.presentation.record.viewmodels.GoalDetailViewModel
 
 class GoalDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGoalDetailBinding
+    private val viewModel by viewModels<GoalDetailViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGoalDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.goalDetailViewModel = viewModel
+        binding.lifecycleOwner = this
+
         goBack()
+        checkComplete()
         goGoalAddActivity()
     }
 
@@ -24,11 +31,29 @@ class GoalDetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkComplete() {
+        viewModel.goalcategory.observe(this) {
+            viewModel.completeDetailCheck()
+        }
+        viewModel.goalresolution.observe(this) {
+            viewModel.completeDetailCheck()
+        }
+        viewModel.goalamount.observe(this) {
+            viewModel.completeDetailCheck()
+        }
+
+        viewModel.isDetailSuccess.observe(this) {
+            binding.btnWrite.isSelected = it
+        }
+    }
+
     private fun goGoalAddActivity() {
         binding.btnWrite.setOnClickListener {
             //나중에 로직 짤 때 editText 다 채웠는지 검사
-            val intent = Intent(this, GoalAddActivity::class.java)
-            startActivity(intent)
+            if (binding.btnWrite.isSelected) {
+                val intent = Intent(this, GoalAddActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
