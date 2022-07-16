5b5bcdb83df229b1401a7867ad5ddba671b93fdd
kimsubeen <papajj06@naver.com>
2022-07-16 20:03:24 +0900

    [feature/#41] refactor위한 파일

--- app/src/main/java/com/teampome/pome/presentation/record/emotion/SelectEmotionViewModel.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/emotion/SelectEmotionViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SelectEmotionViewModel : ViewModel() {
+    val one = MutableLiveData<Boolean>()
+    val two = MutableLiveData<Boolean>()
+    val three = MutableLiveData<Boolean>()
+    private val isCompleted = MutableLiveData<Boolean>()
 
 }
 //button true일 시 해당 textview -> 버튼이 true인가 ? @color/pomesub : @color/pome_grey7
