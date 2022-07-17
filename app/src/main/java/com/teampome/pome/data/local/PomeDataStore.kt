05f021e08316c01cfc3bc834a2feebbb0b900c9c
kimsubeen <papajj06@naver.com>
2022-07-18 02:55:54 +0900

    [feature/#47] login sharedpreference 구현

---
+++ app/src/main/java/com/teampome/pome/data/local/PomeDataStore.kt
@@ -0,0 +1,34 @@
+package com.teampome.pome.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.teampome.pome.BuildConfig
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+const val POME_NAME = "POME"
+
+class PomeDataStore @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    private val dataStore: SharedPreferences =
+        if (BuildConfig.DEBUG) context.getSharedPreferences(POME_NAME, Context.MODE_PRIVATE)
+        else EncryptedSharedPreferences.create(
+            POME_NAME,
+            MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+    var userToken: String
+        set(value) = dataStore.edit { putString("USER_TOKEN", value) }
+        get() = dataStore.getString("USER_TOKEN", "") ?: ""
+
+    var refreshToken: String
+        set(value) = dataStore.edit { putString("REFRESH_TOKEN", value) }
+        get() = dataStore.getString("REFRESH_TOKEN", "") ?: ""
+}
