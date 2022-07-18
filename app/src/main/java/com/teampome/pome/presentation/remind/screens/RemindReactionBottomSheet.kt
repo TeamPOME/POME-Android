7717e9450d17418db165a1cde09cd334a3b2e9e2
yangjiyoung <0112jyoung@naver.com>
2022-07-18 19:50:28 +0900

    [feature/#43] 회고 탭 바텀시트 데이터 연결

--- app/src/main/java/com/teampome/pome/presentation/remind/screens/RemindReactionBottomSheet.kt
+++ app/src/main/java/com/teampome/pome/presentation/remind/screens/RemindReactionBottomSheet.kt
@@ -8,11 +8,14 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentRemindReactionBottomSheetBinding
+import com.teampome.pome.presentation.friends.FriendReactionData
+import com.teampome.pome.presentation.remind.RemindReactionData
+import com.teampome.pome.presentation.remind.adapters.RemindReactionAdapter
 
 class RemindReactionBottomSheet : BottomSheetDialogFragment() {
     private var _binding: FragmentRemindReactionBottomSheetBinding?=null
     private val binding get() = _binding?:error("binding이 되지 않는다.")
-
+    private lateinit var remindReactionAdapter:RemindReactionAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,11 +29,30 @@ class RemindReactionBottomSheet : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initHeight()
+        initAdapter()
+        addData()
+    }
+
+    private fun initAdapter(){
+        remindReactionAdapter= RemindReactionAdapter()
+        binding.rvRemindReaction.adapter=remindReactionAdapter
     }
     private fun initHeight(){
         val bottomSheet = binding.clRemindbottomsheet
         bottomSheet.minHeight = ViewGroup.LayoutParams.WRAP_CONTENT
         bottomSheet.maxHeight = ViewGroup.LayoutParams.WRAP_CONTENT
+    }
+
+    private fun addData(){
+        remindReactionAdapter.submitList(
+            listOf(
+                RemindReactionData(1,"양지영"),
+                RemindReactionData(3,"황연진"),
+                RemindReactionData(5, "김수빈"),
+                RemindReactionData(3,"황연진"),
+                RemindReactionData(5, "김수빈"),
+            )
+        )
     }
 
     override fun onDestroyView() {
