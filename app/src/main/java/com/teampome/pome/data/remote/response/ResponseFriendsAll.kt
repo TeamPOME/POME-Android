7414ae59167655b821da0617ac77522728c43eb3
yangjiyoung <0112jyoung@naver.com>
2022-07-19 01:10:14 +0900

    [feature/#60] 친구 탭 response 수정

--- app/src/main/java/com/teampome/pome/data/remote/response/ResponseFriendsAll.kt
+++ app/src/main/java/com/teampome/pome/data/remote/response/ResponseFriendsAll.kt
@@ -1,14 +1,16 @@
 package com.teampome.pome.data.remote.response
 
 data class ResponseFriendsAll(
-    val _id:String,
-    val name:String,
-    val first_emotion:Int,
-    val second_emotion:Int,
-    val tag:String,
-    val date:String,
-    val description:String,
-    val profile_image:String,
-    val addEmoji:Boolean,
-    val reaction_list:List<Int>
+    val id:String,
+    var userId:Int,
+    val nickname:String,
+    val profileImage:String,
+    val goalMessage:String,
+    val amount:Int,
+    val content:String,
+    val startEmotion:Int,
+    val endEmotion:Int,
+    val timestamp:String,
+    val reaction:List<Int>,
+    val plusCount:Int
 )
