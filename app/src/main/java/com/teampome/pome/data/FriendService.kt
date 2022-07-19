eb1d9723f04745115f87203655329e2a3d286cd9
yangjiyoung <0112jyoung@naver.com>
2022-07-19 15:50:53 +0900

    [feature/#60] friendservice 구현

--- app/src/main/java/com/teampome/pome/data/FriendService.kt
+++ app/src/main/java/com/teampome/pome/data/FriendService.kt
@@ -1,8 +1,13 @@
 package com.teampome.pome.data
 
+import com.teampome.pome.data.remote.response.BaseResponse
+import com.teampome.pome.data.remote.response.ResponseFriendsAll
 import retrofit2.http.GET
 
 interface FriendService {
-    @GET()
-    suspend fun
+    @GET("friends")
+    suspend fun getAllFriends(): BaseResponse<ResponseFriendsAll>
+
+    @GET("friends/records")
+    suspend fun getFriendsRecords():BaseResponse<ResponseFriendsAll>
 }
