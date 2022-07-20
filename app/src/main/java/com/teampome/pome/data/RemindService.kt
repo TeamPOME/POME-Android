f3f609a2c3cfb80e6979923ae8003795a9640f14
yangjiyoung <0112jyoung@naver.com>
2022-07-21 01:03:20 +0900

    [feature/#65] 회고 탭 회고 서비스 작성

--- app/src/main/java/com/teampome/pome/data/RemindService.kt
+++ app/src/main/java/com/teampome/pome/data/RemindService.kt
@@ -1,8 +1,15 @@
 package com.teampome.pome.data
 
+import com.teampome.pome.data.remote.response.BaseResponse
+import com.teampome.pome.data.remote.response.ResponseRemindData
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RemindService {
-    @GET
-    suspend fun getRemindData()
+    @GET("records")
+    suspend fun getRemindData(
+        @Query("goalId")goalId:Int,
+        @Query("startEmotion")startEmotion:Int,
+        @Query("endEmotion")endEmotion:Int
+    ):BaseResponse<List<ResponseRemindData>>
 }
