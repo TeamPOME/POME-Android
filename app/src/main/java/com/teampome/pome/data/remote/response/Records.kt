b5b778028edc899c4e01022140b1db0a6a3b18e4
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-21 03:21:50 +0900

    [fix/#64] response 객체 records 오류 해결

---
+++ app/src/main/java/com/teampome/pome/data/remote/response/Records.kt
@@ -0,0 +1,10 @@
+package com.teampome.pome.data.remote.response
+
+data class Records(
+    val date: String,
+    val amount: Int,
+    val content: String,
+    val startEmotion: Int,
+    val endEmotion: Int
+)
+
--- app/src/main/java/com/teampome/pome/data/remote/response/ResponseRecordsLook.kt
+++ app/src/main/java/com/teampome/pome/data/remote/response/ResponseRecordsLook.kt
@@ -1,9 +1,6 @@
 package com.teampome.pome.data.remote.response
 
 data class ResponseRecordsLook(
-    val date: String,
-    val amount: Int,
-    val content: String,
-    val startEmotion: Int,
-    val endEmotion: Int
+    val incompleteTotal: Int,
+    val records: List<Records>
 )
