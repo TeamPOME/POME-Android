686f7bbf9678bb7b7015851a1df0fcf6c22e865f
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-22 06:06:39 +0900

    [feature/#75] 바텀시트 UI 생성

---
+++ app/src/main/java/com/teampome/pome/presentation/record/screens/RecordBottomSheet.kt
@@ -0,0 +1,20 @@
+package com.teampome.pome.presentation.record.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.teampome.pome.R
+
+class RecordBottomSheet : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record_bottom_sheet, container, false)
+    }
+
+}
