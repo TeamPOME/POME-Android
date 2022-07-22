1a60d4936e4ac2abd8aabea8fc9338fd8a5a9b98
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-07-22 21:01:53 +0900

    [chore/#75] 필요없는 import 삭제

--- app/src/main/java/com/teampome/pome/presentation/record/screens/GoalDeleteBottomSheet.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/screens/GoalDeleteBottomSheet.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentGoalDeleteBottomSheetBinding
 import com.teampome.pome.util.setOnSingleClickListener
 
